ea7d6f380443a9b512f82b8f9fbeb2cac1639b12
vovchik851 <vovchik851@gmail.com>
2021-05-15 01:08:48 +0300

    1

---
+++ js/modyle1.js
@@ -0,0 +1,60 @@
+
+// #1
+//const productName = "Droid";
+//console.log(productName);
+//const pricePerItem = 2000;
+//console.log(pricePerItem);
+
+//#2
+//let productName = "Droid";
+//let pricePerItem = 2000;
+//productName = "Repair droid"
+//pricePerItem = 2000 + 1500;
+
+//#3
+//const topSpeed = 160;
+//const distance = 617.54;
+//const login = "mango935";
+//const isOnline = true;
+//const isAdmin = false;
+
+//#4
+//const pricePerItem = 3500;
+//const orderedQuantity = 4;
+//const totalPrice = pricePerItem *orderedQuantity;
+//console.log(3500 * 4);
+
+//#5
+//const productName = "Droid";
+//const pricePerItem = 3500;
+//const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+//console.log(message);
+
+//#6
+const orderedQuantity = 6;
+const pricePerDroid = 800;
+const deliveryFee = 50;
+let totalPrice;
+console.log(totalPrice = orderedQuantity * pricePerDroid + deliveryFee);
+const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+console.log(message);
+
+
+//#7
+//#8
+//#9
+//#10
+//#11
+//#12
+//#13
+//#14
+//#15
+//#16
+//#17
+//#18
+//#19
+//#20
+//#21
+//#22
+//#23
+//#24
