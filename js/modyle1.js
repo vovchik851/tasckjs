5ff7ea866ce22550cbaedc60e0e82ab3066681df
vovchik851 <vovchik851@gmail.com>
2021-05-18 00:26:36 +0300

    Update modyle1.js

--- js/modyle1.js
+++ js/modyle1.js
@@ -31,24 +31,117 @@
 //console.log(message);
 
 //#6
-const orderedQuantity = 6;
-const pricePerDroid = 800;
-const deliveryFee = 50;
-let totalPrice;
-console.log(totalPrice = orderedQuantity * pricePerDroid + deliveryFee);
-const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
-console.log(message);
+//const orderedQuantity = 6;
+//const pricePerDroid = 800;
+//const deliveryFee = 50;
+//let totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//console.log(totalPrice);
+//const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//console.log(message);
 
 
 //#7
+//function sayHi() {
+//console.log("Hello, this is my first function!");
+//}
+//sayHi(); 
+//sayHi(); 
+//sayHi();
+
 //#8
+//function add(a, b, c) {
+//console.log(`Addition result equals ${a + b + c}`);
+//}
+//add(15, 27, 10);
+//add(10, 20, 30);
+//add(5, 10, 15);
+
 //#9
+//function add(a, b, c) {
+//return a + b + c;
+//}
+//add(2, 5, 8); // 15
+//let result = add(15, 27, 10);
+//console.log(result);
+//result = add(10, 20, 30);
+//console.log(result);
+//result = add(5, 10, 15);
+//console.log(result);
+
 //#10
+//function makeMessage (name, price) {
+//const message = (`You picked ${name}, price per item is ${price} credits`);
+//return message;
+//};
+//let result = makeMessage("Radar", 6150);
+//console.log(result);
+//result = makeMessage("Scanner", 3500);
+//console.log(result);
+//result = makeMessage("Reactor", 8000);
+//console.log(result);
+//result = makeMessage("Engine", 4070);
+//console.log(result);
+
 //#11
+//function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//const totalPrice = orderedQuantity*pricePerItem;
+//return totalPrice;
+//};
+//let result = calculateTotalPrice(5, 100);
+//console.log(result);
+//result = calculateTotalPrice(8, 60);
+//console.log(result);
+//result = calculateTotalPrice(3, 400);
+//console.log(result);
+//result = calculateTotalPrice(1, 3500);
+//console.log(result);
+//result = calculateTotalPrice(12, 70);
+//console.log(result);
+
 //#12
+//function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//const message = (`You ordered droids worth  ${orderedQuantity * pricePerDroid + deliveryFee} credits. Delivery (${deliveryFee} credits) is included in total price.`);
+//return message;
+//}
+//let result = makeOrderMessage(2, 100, 50);
+//console.log(result);
+//result = makeOrderMessage(4, 300, 100);
+//console.log(result);
+//result = makeOrderMessage(10, 70, 200);
+//console.log(result);
+
 //#13
+//function isAdult(age) {
+//const passed = age >= 18;
+//return passed;
+//}
+//let result = isAdult(20);
+//console.log(result);
+//result = isAdult(14);
+//console.log(result);
+//result = isAdult(8);
+//console.log(result);
+//result = isAdult(37);
+//console.log(result);
+
 //#14
+//function isValidPassword(password) {
+//const SAVED_PASSWORD = 'jqueryismyjam';
+//const isMatch = password === SAVED_PASSWORD;
+//return isMatch;
+//}
+//let result = isValidPassword("mangodab3st");
+//console.log(result);
+//result = isValidPassword("kiwirul3z");
+//console.log(result);
+//result = isValidPassword("jqueryismyjam");
+//console.log(result);
+
 //#15
+
+
+
+
 //#16
 //#17
 //#18
@@ -58,3 +151,15 @@ console.log(message);
 //#22
 //#23
 //#24
+//#25
+//#26
+//#27
+//#28
+//#29
+//#30
+//#31
+//#32
+//#33
+//#34
+//#35
+//#36
