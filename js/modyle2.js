f3e4859b27e7c0ca26532827bede55cfd0fd104a
vovchik851 <vovchik851@gmail.com>
2021-05-22 00:48:41 +0300

    Update modyle2.js

---
+++ js/modyle2.js
@@ -0,0 +1,103 @@
+//№ 1
+
+//function checkAge(age) {
+  //if (age>=18) { 
+    //return "You are an adult";
+//}
+
+//return "You are a minor";
+//}
+
+//№ 2
+
+//function checkPassword(password) {
+  //const ADMIN_PASSWORD = "jqueryismyjam";
+
+  //if (password === ADMIN_PASSWORD) {
+    //return "Welcome!";
+    //}
+    
+    //return "Access denied, wrong password!";
+//}
+
+
+//№ 3
+
+function checkStorage(available, ordered) {
+
+  if (ordered === 0) {
+    return "Your order is empty!";
+  } if (ordered > available) {
+    return "Your order is too large, not enough goods in stock!";
+  } 
+    return "The order is accepted, our manager will contact you";
+  // Change code above this line
+}
+
+
+//№ 4
+
+//№ 5
+
+//№ 6
+
+//№ 7
+
+//№ 8
+
+//№ 9
+
+//№ 10
+
+//№ 11
+
+//№ 12
+
+
+//№ 13
+
+
+//№ 14
+
+
+//№ 15
+
+
+//№ 16
+
+
+//№ 17
+
+
+//№ 18
+
+
+//№ 19
+
+
+//№ 20
+
+
+//№ 21
+
+
+//№ 22
+
+//№ 23
+
+//№ 24
+
+//№ 25
+
+//№ 26
+
+//№ 27
+
+//№ 28
+
+//№ 29
+
+//№ 31
+
+//№ 32
+
