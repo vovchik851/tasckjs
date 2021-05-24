cda104378d3a3d2ba146d9f5c83cd592f53330db
vovchik851 <vovchik851@gmail.com>
2021-05-25 00:49:11 +0300

    1

--- js/modyle2.js
+++ js/modyle2.js
@@ -23,34 +23,72 @@
 
 //№ 3
 
-function checkStorage(available, ordered) {
+//function checkStorage(available, ordered) {
 
-  if (ordered === 0) {
-    return "Your order is empty!";
-  } if (ordered > available) {
-    return "Your order is too large, not enough goods in stock!";
-  } 
-    return "The order is accepted, our manager will contact you";
-  // Change code above this line
-}
-
+  //if (ordered === 0) {
+    //return "Your order is empty!";
+  //} if (ordered > available) {
+    //return "Your order is too large, not enough goods in stock!";
+  //} 
+    //return "The order is accepted, our manager will contact you";
+//}
 
 //№ 4
 
+//const fruits = ["apple", "plum", "pear", "orange"];
+
 //№ 5
+
+//const fruits = ["apple", "plum", "pear", "orange"];
+//const firstElement = fruits[0];//первый элемент массива
+//const secondElement = fruits[1];//второй элемент массива
+//const lastElement = fruits[3];//последний элемент массива
 
 //№ 6
 
+//const fruits = ["apple", "plum", "pear", "orange"];
+//fruits[1] = "peach";
+//fruits[3] = "banana";
+
 //№ 7
+
+//const fruits = ["apple", "peach", "pear", "banana"];
+//const fruitsArrayLength = (fruits.length);
 
 //№ 8
 
+//const fruits = ["apple", "peach", "pear", "banana"];
+//const lastElementIndex = fruits.length - 1;
+//const lastElement = fruits[lastElementIndex];
+
 //№ 9
 
+//function getExtremeElements(array) {
+ //const firstElement = array[0];
+  //const lastElementIndex = array.length - 1;
+  //const lastElement = array[lastElementIndex];
+//return [firstElement, lastElement];
+//}
+
+//let message = (getExtremeElements([1, 2, 3, 4, 5]));
+//console.log(message);
+
 //№ 10
+//function splitMessage(message, delimeter) {
+  //let words;
+//words = message.split(delimeter);
+  //return words;
+//}
+//let message = splitMessage("Mango hurries to the train", " ");
+//console.log(message);
 
 //№ 11
 
+//function calculateEngravingPrice(message, pricePerWord) {
+    //return message.split(" ").length * pricePerWord;
+//}
+//let message=calculateEngravingPrice("JavaScript is in my blood", 10)
+//console.log(message);
 //№ 12
 
 
