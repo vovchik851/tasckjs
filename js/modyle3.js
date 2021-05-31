bda8dda62aabd70176c0e84153bc15d2c025a930
vovchik851 <vovchik851@gmail.com>
2021-06-01 00:40:09 +0300

    JS

--- js/modyle3.js
+++ js/modyle3.js
@@ -9,29 +9,202 @@
 
 //#2
 
-
+//const apartment = {//
+    //imgUrl: 'https://via.placeholder.com/640x480',
+    //descr: 'Spacious apartment in the city center',
+    //rating: 4,
+    //price: 2153,
+//     tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//   name: "Henry",
+//   phone: "982-126-1588",
+//   email: "henry.carter@aptmail.com"}
+//   };
 
 //#3
 
-
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// }
+// const aptRating = apartment.rating;
+// const aptDescr = apartment.descr;
+// const aptPrice = apartment.price;
+// const aptTags = apartment.tags;
 
 //#4
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// const ownerName = apartment.owner.name;
+// console.log(ownerName);
+// const ownerPhone = apartment.owner.phone;
+// console.log(ownerPhone);
+// const ownerEmail = apartment.owner.email;
+// console.log(ownerEmail);
+// const numberOfTags = apartment.tags.length;
+// console.log(numberOfTags);
+// const firstTag = apartment.tags[0];
+// console.log(firstTag);
+// const lastTag = apartment.tags[2];
+// console.log(lastTag);
+
 //#5
+
+
+
 //#6
+
+
+
 //#7
+
+
+
 //#8
+
+
+
 //#9
+
+
+
 //#10
+
+
+
 //#11
+
+
+
 //#12
+
+
+
 //#13
+
+
+
 //#14
+
+
+
 //#15
+
+
+
 //#16
+
+
+
 //#17
+
+
+
 //#18
+
+
+
 //#19
+
+
+
 //#20
+
+
+
 //#21
+
+
+
 //#22
+
+
+
 //#23
+
+
+
+//#24
+
+
+
+//#25
+
+
+
+//#26
+
+
+
+//#27
+
+
+
+//#28
+
+
+
+//#29
+
+
+
+//#30
+
+
+
+//#31
+
+
+
+//#32
+
+
+
+//#33
+
+
+
+//#34
+
+
+
+//#35
+
+
+
+//#36
+
+
+
+//#37
+
+
+
+//#38
+
+
+
+//#39
+
+
+
+//#40
+
+
+
+//#41
