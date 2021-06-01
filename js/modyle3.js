4a62c167fd7dacbca3e20ff09c354e513ecdb5fe
vovchik851 <vovchik851@gmail.com>
2021-06-01 23:29:54 +0300

    Update modyle3.js

--- js/modyle3.js
+++ js/modyle3.js
@@ -65,37 +65,145 @@
 
 //#5
 
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
 
+// const aptRating = apartment['rating'];
+// const aptDescr = apartment['descr'];
+// const aptPrice = apartment['price'];
+// const aptTags = apartment['tags'];
+// console.log(aptRating);
+// console.log(aptDescr);
+// console.log(aptPrice);
+// console.log(aptTags);
 
-//#6
+//#6 Изменение свойств
 
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = 'Henry Sibola';
+// apartment.tags.push('trusted');
+// console.log(apartment.price);
+// console.log(apartment.rating);
+// console.log(apartment.owner.name);
+// console.log(apartment.tags);
 
+//#7 Добавление свойств
 
-//#7
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
 
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {
+//     country: "Jamaica",
+//     city : "Kingston",
+// }
+// console.log(apartment.area);
+// console.log(apartment.rooms);
+// console.log(apartment.location);
 
+//#8 Короткие свойства
 
-//#8
+// const name = 'Repair Droid';
+// const price = 2500;
+// const image = 'https://via.placeholder.com/640x480';
+// const tags = ['on sale', 'trending', 'best buy'];
 
+// const product = {
+//     name,
+//     price,
+//     image,
+//     tags
+// };
+// console.log(product);
 
+//#9 Вычисляемые свойства
 
-//#9
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
 
+// const credentials = {
+//     [emailInputName]: "henry.carter@aptmail.com",
+//     [passwordInputName]: "jqueryismyjam"
+// };
 
+//#10 Цикл for...in
 
-//#10
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// for (const key in apartment) {
+//     keys.push(key);
+// values.push(apartment[key]);
+// }
 
+//#11 Метод hasOwnProperty()
 
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
 
-//#11
+// for (const key in apartment) {
+//     if (apartment.hasOwnProperty(key)) {
+//         keys.push(key);
+//         values.push(apartment[key]);
+//     }
+// }
+// console.log(keys);
+// console.log(values);
+// console.log(apartment.service);
 
+//#12 Задача. Подсчёт свойств
 
+// function countProps(object) {
+//   let propCount = 0;
+// for (const obj in object) {
+//     if (object.hasOwnProperty(obj)) {
+//         propCount += 1;
+//     }
+// }
+//   return propCount;
+// }
 
-//#12
-
-
-
-//#13
+//#13 Метод Object.keys()
 
 
 
