d2d4fe94604dcdeaac883012c3c9b0fbd111e9b3
vovchik851 <vovchik851@gmail.com>
2021-06-08 19:46:04 +0300

    Update modyle3.js

--- js/modyle3.js
+++ js/modyle3.js
@@ -305,51 +305,156 @@
 //   return arrProducts;
 // }
 
-
-
 //#20
 
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
 
+// function calculateTotalPrice(productName) {
+//     let totalPrice = 0;
+//     for (const product of products) {
+//         if (productName === product.name) {
+//             totalPrice = product.price*product.quantity;
+//         }
+//     }
+//     return totalPrice;
+// }
+// let result = calculateTotalPrice("Grip");
+// console.log(result);
 
-//#21
+//#21 Деструктуризация объектов
 
-
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// const { yesterday, today, tomorrow } = highTemperatures;
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
 
 //#22
 
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// const { yesterday, today, tomorrow, icon =
+//     "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } = highTemperatures;
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+//#23 Изменение имени переменной
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// const { yesterday: highYesterday, today: highToday,
+//     tomorrow: highTomorrow, icon:
+//     highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"
+// } = highTemperatures;
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+//#24 Деструктуризация в циклах
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+// const hexColors = [];
+// const rgbColors = [];
+// for (const{hex, rgb} of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
+//#25 Глубокая деструктуризация
+
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// const{ today:
+//     { high: highToday,
+//         low: lowToday,
+//         icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg"
+// },
+//     tomorrow: {
+//         high: highTomorrow,
+//         low: lowTomorrow,
+//         icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"
+//     } } = forecast;
+
+//#26 Паттерн «Объект настроек»
+
+// function calculateMeanTemperature(forecast) {
+//     const { today: { low:todayLow, high:todayHigh },
+//     tomorrow: { low:tomorrowLow, high:tomorrowHigh }
+// } = forecast;
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+
+//#27 Операция spread при передаче аргументов
+
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = scores;
+// const worstScore = scores;
+
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
+
+//#28 Операция spread при создании нового массива
+
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+// const bestScore =  Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
 
 
-//#23
+//#29 Операция spread при создании нового объекта
 
+// const defaultSettings = {
+//   theme: 'light',
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// const finalSettings = {...defaultSettings, ...overrideSettings};
 
+//#30 Задача. Карточки задач
 
-//#24
+function makeTask(data) {
+  const completed = false;
+  const category = 'General';
+  const priority = 'Normal';
+  // Change code below this line
 
-
-
-//#25
-
-
-
-//#26
-
-
-
-//#27
-
-
-
-//#28
-
-
-
-//#29
-
-
-
-//#30
-
-
+  // Change code above this line
+}
 
 //#31
 
